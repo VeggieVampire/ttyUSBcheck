f927319c64a0a47822874c61975d25472741c40c
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-12-08 00:54:44 -0600

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,10 @@
+linesTOread=200
+#check TTY connections
+TTYUSB=$(dmesg | grep tty|grep USB|rev|awk '{print $1}'|rev)
+
+echo -n "USB TTY device found at /dev/"
+echo -n $TTYUSB
+echo.
+read -n $linesTOread LINES < /dev/$TTYUSB
+echo "Reading Lines:"
+echo $LINES
